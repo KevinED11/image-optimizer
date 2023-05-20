6b11e3de7606ea8f4913a6ef35e77efaf4006d14
KevinED11 <mendez_kael@hotmail.com>
2023-05-19 19:26:12 -0700

    none

--- main.py
+++ main.py
@@ -7,16 +7,35 @@ class DirectoryNotFound(Exception):
     pass
 
 
-def source_directory(directory: str) -> list[str]:
-    dir_path = os.path.abspath(directory)  
-    return os.listdir(dir_path)
+class DirectoryIsEmpty(Exception):
+    pass
 
 
-def showImage(image_path: str) -> None:
+def get_files(directory: str) -> list[str]:
+    dir_path = os.path.abspath(directory)
+    if not os.path.exists(dir_path):
+        raise DirectoryNotFound(f"Directory not found: {dir_path}")
+    files = os.listdir(dir_path)
+    if not files:
+        raise DirectoryIsEmpty("This directory is empty")
+
+    return files
+
+
+def filter_files(files: list[str]) -> list[str]:
+    if valid_files := [file for file in files
+                       if os.path.splitext(file)[1]
+                       in [".jpg", ".webp", ".png"]]:
+        return valid_files
+
+    raise DirectoryIsEmpty("This directory is empty")
+
+
+def show_image(image_path: str) -> None:
     dir_path = os.path.abspath(image_path)
     with Image.open(dir_path) as image:
         image.show()
-    
+
 
 def images_metadata(images: list[str]) -> dict[str, list]:
     data = {}
@@ -29,8 +48,9 @@ def images_metadata(images: list[str]) -> dict[str, list]:
 
     return data
 
-  
-def optimize_images(images: list[str], from_directory: str, to_directory: str, quality: float = 50) -> None:
+
+def optimize_images(images: list[str], from_directory: str,
+                    to_directory: str, quality: float = 50) -> None:
     from_dir_path = os.path.abspath(from_directory)
     to_dir_path = os.path.abspath(to_directory)
 
@@ -39,35 +59,43 @@ def optimize_images(images: list[str], from_directory: str, to_directory: str, q
     for name_image in images:
         with Image.open(os.path.join(from_dir_path, name_image)) as img:
             file_name = f"optimized_{name_image}"
-            img.save(to_dir_path + "/" + file_name, optimize=True, quality=quality)
+            img.save(to_dir_path + "/" + file_name,
+                     optimize=True, quality=quality)
 
-                      
-def get_images_size(images: list[str], directory: str) -> dict[str, float]:
+
+def get_images_size(images: list[str], directory: str) -> dict[str, str]:
+    if not images:
+        pass
     dir_path = os.path.abspath(directory)
     sizes = {}
     for image in images:
-        sizes[image] = humanize.naturalsize(os.path.getsize(os.path.join(dir_path, image)))
+        sizes[image] = humanize.naturalsize(
+            os.path.getsize(os.path.join(dir_path, image)))
 
     return sizes
 
 
-
 def main():
-    images = source_directory(directory="images")
-    images2 = source_directory(directory="dist")
-  
+    try:
+        images = get_files(directory="images")
+        images2 = get_files(directory="dist")
+        filter = filter_files(images)
+        filter2 = filter_files(images2)
 
-    metadata = images_metadata(images)
-    print(metadata)
+        metadata = images_metadata(images)
+        print(metadata)
 
-    optimize_images(images, from_directory="images",
-                    to_directory="/home/kevind/Documentos/Workspace", quality=10)
-    
-    sizes = get_images_size(images, directory="images")
-    sizes2 = get_images_size(images2, directory="dist")
-    print(sizes)
-    print(sizes2)
-    
+        optimize_images(filter, from_directory="images",
+                        to_directory="/home/kevind/Documentos/Workspace",
+                        quality=50)
+
+        sizes = get_images_size(filter, directory="images")
+        sizes2 = get_images_size(filter2, directory="dist")
+        print(sizes)
+        print(sizes2)
+    except (DirectoryNotFound, DirectoryIsEmpty) as err:
+        print(err)
+
 
 if __name__ == "__main__":
     main()
